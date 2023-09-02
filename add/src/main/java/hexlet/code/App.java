44fa647a13efecf24875580c80cc38f2cfce3fa9
Dasha <redoduvan@bk.ru>
2023-09-02 21:17:10 +0300

    add new class Even and files

--- add/src/main/java/hexlet/code/App.java
+++ add/src/main/java/hexlet/code/App.java
@@ -1,8 +1,45 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
+    String nameUser;
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.sayHello();
+        chooseGame();
+        //Cli.sayHello();
+
+    }
+
+    static void chooseGame() {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        Scanner scan = new Scanner(System.in);
+        String miniGame = scan.nextLine();
+        runGame(miniGame);
+    }
+    static void runGame(String miniGame) {
+        App userApp = new App();
+        Cli.sayHello(userApp);
+
+        switch (miniGame) {
+            case ("1") -> Cli.sayHello(userApp);
+            case ("2") -> Even.run(userApp.getNameUser());
+            case ("0") -> {
+                break;
+            }
+            default -> System.out.println("It's wrong option!");
+        }
     }
 }
--- add/src/main/java/hexlet/code/Cli.java
+++ add/src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,13 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void sayHello() {
+    public static void sayHello(App userApp) {
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner scan = new Scanner(System.in);
-        String nameUser = scan.nextLine();
-        System.out.println("Hello, " + nameUser);
+        //App userApp = new App();
+        userApp.setNameUser(scan.nextLine());
+
+        System.out.println("Hello, " + userApp.getNameUser());
     }
 }
