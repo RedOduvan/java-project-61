4486f14367f86340daed4fbaa2e731fb6db7db86
Dasha <redoduvan@bk.ru>
2023-09-03 14:33:55 +0300

    add new class Prime

--- add/src/main/java/hexlet/code/App.java
+++ add/src/main/java/hexlet/code/App.java
@@ -4,6 +4,7 @@ import hexlet.code.games.Calculator;
 import hexlet.code.games.Even;
 import hexlet.code.games.Gcd;
 import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 
 import java.util.Scanner;
 
@@ -20,7 +21,6 @@ public class App {
 
     public static void main(String[] args) {
         chooseGame();
-        //Cli.sayHello();
 
     }
 
@@ -31,6 +31,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         Scanner scan = new Scanner(System.in);
@@ -47,6 +48,7 @@ public class App {
             case ("3") -> Calculator.run(userApp.getNameUser());
             case ("4") -> Gcd.run(userApp.getNameUser());
             case ("5") -> Progression.run(userApp.getNameUser());
+            case ("6") -> Prime.run(userApp.getNameUser());
             case ("0") -> {
                 break;
             }
---
+++ add/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,37 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class Prime {
+    public static boolean isSimple(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i < number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void run(String nameUser) {
+        int randomNum;
+        Engine en = new Engine();
+        en.setNameUser(nameUser);
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
+        en.setExitLoop(true);
+        while (en.isExitLoop()) {
+            randomNum = (int) (Math.random() * 1000);
+            en.setQuestion(String.valueOf(randomNum));
+            en.setRightAnswer("no");
+            if (isSimple(randomNum)) {
+                en.setRightAnswer("yes");
+            }
+
+            Engine.askQuestion(en);
+            Engine.getAnswer(en);
+            Engine.check(en);
+        }
+    }
+}
